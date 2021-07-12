97336b58e962798d30863617fba0c8acd336f97d
bigboss <kursus1010@gmail.com>
2021-07-12 22:02:41 +0200

    filter and search django rest api

--- backend/vueapp/views.py
+++ backend/vueapp/views.py
@@ -7,9 +7,12 @@ from .serializers import ContactBookSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class ContactBookViewSet(viewsets.ModelViewSet):
     authentication_classes = (BasicAuthentication,)
     permission_classes = (IsAuthenticated, )
     queryset = ContactBook.objects.all()
     serializer_class = ContactBookSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('firstname', 'lastname', 'department')
