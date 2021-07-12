b8bf7ffd353b6ddbe7480b4606d672a7da02de2b
bigboss <kursus1010@gmail.com>
2021-07-12 18:14:58 +0200

    refactoring frontend

--- frontend/vueapp/src/App.vue
+++ frontend/vueapp/src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <style lang="scss">
-@import '../../node_modules/bulma';
+@import '../node_modules/bulma';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
