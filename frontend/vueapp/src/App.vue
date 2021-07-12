8307391614a3619969f69ec6c7ea72e65008264d
bigboss <kursus1010@gmail.com>
2021-07-12 19:13:01 +0200

    testing vue js

--- frontend/vueapp/src/App.vue
+++ frontend/vueapp/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <button class="button is-primary">Primary Btn</button>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
-@import '../node_modules/bulma';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
